0371bda3149ccc0e8c3a8347a2430e72a8f2b8b8
YimonA <yimonaye997@gmail.com>
2023-08-29 10:25:30 +0630

    update login design

--- src/components/MediaImgDetail.jsx
+++ src/components/MediaImgDetail.jsx
@@ -1,3 +1,5 @@
+import { MdClose } from "react-icons/md";
+import PropTypes from "prop-types";
 import { MdArrowBackIosNew } from "react-icons/md";
 import { MdArrowForwardIos } from "react-icons/md";
 import { RiDeleteBinLine } from "react-icons/ri";
@@ -9,7 +11,14 @@ import Cookies from "js-cookie";
 import { addPhotos } from "../redux/services/mediaSlice";
 import { useContextCustom } from "../context/stateContext";
 
-const MediaImgDetail = ({ imgIndex, imgDetail, imgs }) => {
+const MediaImgDetail = ({show, imgIndex, imgDetail, imgs }) => {
+  MediaImgDetail.propTypes = {
+    show:PropTypes.bool,
+    imgIndex:PropTypes.number,
+    imgDetail:PropTypes.object,
+    imgs: PropTypes.array,
+  };
+
   const { setShowModal } = useContextCustom();
   const [deletePhoto] = useDeletePhotoMutation();
   const dispatch = useDispatch();
@@ -29,6 +38,17 @@ const MediaImgDetail = ({ imgIndex, imgDetail, imgs }) => {
   useEffect(() => {
     dispatch(addPhotos({ photos: imgs }));
   }, [imgs]);
+
+  useEffect(() => {
+    if (pictureIndex === imgAry.length - 1) {
+      setShowNext(false);
+    } else if (pictureIndex === 0) {
+      setShowPrev(false);
+    } else if (pictureIndex < imgAry.length && pictureIndex > 0) {
+      setShowNext(true);
+      setShowPrev(true);
+    }
+  }, [pictureIndex]);
 
   const deletePhotoHandler = async (id) => {
     const { data } = await deletePhoto({ id, token });
@@ -55,57 +75,74 @@ const MediaImgDetail = ({ imgIndex, imgDetail, imgs }) => {
     if (pictureIndex < imgAry.length - 1) {
       setPictureDetail(imgs[pictureIndex + 1]);
       setPictureIndex(pictureIndex + 1);
-      // console.log("hh", pictureDetail);
-    } else if (pictureIndex === imgAry.length - 1) {
-      setShowNext(false);
-      // console.log("hh", pictureDetail);
     }
   };
 
   const prevBtn = () => {
-    if (pictureIndex < imgAry.length - 1 && pictureIndex) {
+    if (pictureIndex < imgAry.length && pictureIndex != 0) {
       setPictureDetail(imgs[pictureIndex - 1]);
       setPictureIndex(pictureIndex - 1);
-      // console.log("yy", pictureDetail);
-    }
-    if (pictureIndex === 0) {
-      setShowPrev(false);
-      // console.log("yy", pictureDetail, pictureIndex);
     }
   };
 
   return (
-    <div className=" bg-[--base-color] py-10 ">
-      <div className=" flex justify-between items-center mb-10">
-        <MdArrowBackIosNew
-          onClick={prevBtn}
-          className={` ${
-            showPrev ? "block" : "opacity-0"
-          }  text-[--secondary-color] cursor-pointer mx-2`}
-          size={"1.5rem"}
-        />
-        <img src={pictureDetail.url} className="w-[320px] h-[300px]" alt="" />
-        <MdArrowForwardIos
-          onClick={nextBtn}
-          className={` ${
-            showNext ? "block" : "opacity-0"
-          } text-[--secondary-color] cursor-pointer mx-2`}
-          size={"1.5rem"}
-        />
-      </div>
-      <div className="w-[60px] h-[50px] mx-auto flex justify-center items-center gap-3">
-        <button
-          onClick={() => deletePhotoHandler(pictureDetail?.id)}
-          className={`text-[--base-color] basis-1/2 bg-[--secondary-color] p-1 rounded-[5px] `}
-        >
-          <RiDeleteBinLine size={"1.5rem"} />
-        </button>
-        <button
-          onClick={() => navigator.clipboard.writeText(pictureDetail?.url)}
-          className={`text-[--base-color] basis-1/2 bg-[--secondary-color] p-1 rounded-[5px] `}
-        >
-          <BiCopy size={"1.5rem"} />
-        </button>
+    <div
+      className={`${
+        show ? "flex" : "hidden"
+      } justify-center items-center absolute top-0 left-0 w-screen h-full  `}
+    >
+      <div className="z-20 absolute top-0 left-0 h-screen w-screen bg-black opacity-70"></div>
+      <div className="z-50 min-w-[500px] min-h-[500px] flex flex-col justify-start items-center bg-[var(--sidebar-color)] border border-[var(--border-color)]">
+        <div className="w-full h-[60px] px-5 flex justify-between items-center gap-10 bg-black mb-10">
+          <span className="select-none text-[18px] font-semibold text-[var(--secondary-color)] text-center">
+            {pictureDetail?.name}
+          </span>
+          <MdClose
+            onClick={() => setShowModal(false)}
+            size={"1.5rem"}
+            className="cursor-pointer text-[var(--secondary-color)]"
+          />
+        </div>
+
+        {/* mordal body start */}
+        <div className="w-full h-full bg-[--base-color] py-10 ">
+          <div className=" flex justify-between items-center mb-10">
+            <MdArrowBackIosNew
+              onClick={prevBtn}
+              className={` ${
+                showPrev ? "block" : "opacity-0"
+              }  text-[--secondary-color] cursor-pointer mx-2`}
+              size={"1.5rem"}
+            />
+            <img
+              src={pictureDetail.url}
+              className="w-[320px] h-[300px]"
+              alt=""
+            />
+            <MdArrowForwardIos
+              onClick={nextBtn}
+              className={` ${
+                showNext ? "block" : "opacity-0"
+              } text-[--secondary-color] cursor-pointer mx-2`}
+              size={"1.5rem"}
+            />
+          </div>
+          <div className="w-[60px] h-[50px] mx-auto flex justify-center items-center gap-3">
+            <button
+              onClick={() => deletePhotoHandler(pictureDetail?.id)}
+              className={`text-[--base-color] basis-1/2 bg-[--secondary-color] p-1 rounded-[5px] `}
+            >
+              <RiDeleteBinLine size={"1.5rem"} />
+            </button>
+            <button
+              onClick={() => navigator.clipboard.writeText(pictureDetail?.url)}
+              className={`text-[--base-color] basis-1/2 bg-[--secondary-color] p-1 rounded-[5px] `}
+            >
+              <BiCopy size={"1.5rem"} />
+            </button>
+          </div>
+        </div>
+        {/* mordal body end */}
       </div>
     </div>
   );
